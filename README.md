fb6bfe7d16ad7234482cbcac451d7c085af8148f
Bucky Maler <buckymaler@Buckys-iMac.local>
2016-12-22 00:37:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# [Jekyll Starter Kit](https://buckymaler.github.io/jekyll-starter-kit)
+
+Ground zero for your next Jekyll website.
+
+  - Sass is transpiled, autoprefixed, and minified
+  - JavaScript is hinted, concatenated, and uglified
+  - Images are optimized
+  - Production files are updated on save and the browser will refresh automagically
+
+## Getting started
+
+  - clone the repo
+  - `npm install -g gulp`
+  - `npm install`
+  - `gulp`
+  - Live [http://localhost:3000](http://localhost:3000)
+
+## Available commands
+
+  - `gulp` compile all src to site, watch files, and auto refresh
+  - `gulp clean` wipe out site/assets entirely
+
+## Publishing
+
+  - Disconnect your local repo from this one with `git remote remove origin`
+  - `hub create`
+  - add, commit, and push
+
+## Credits
+
+  - [DevTips Starter Kit](https://github.com/DevTips/DevTips-Starter-Kit) visual design heavily influenced the Jekyll Starter Kit visual design
+
+## Misc:
+
+  - Follow Bucky: [Twitter](https://twitter.com/BuckyMaler), [GitHub](https://github.com/BuckyMaler)
---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+/*
+ Enjoy Jekyll with the streamlined optimizations of Gulp.
+*/
+
+// node modules required
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		autoprefix = require('gulp-autoprefixer'),
+		jsHint = require('gulp-jshint'),
+		concat = require('gulp-concat'),
+		uglify = require('gulp-uglify'),
+		plumber = require('gulp-plumber'),
+		rename = require('gulp-rename'),
+		imageMin = require('gulp-imagemin'),
+		cache = require('gulp-cache'),
+		del = require('del'),
+    cp = require('child_process'),
+		browserSync = require('browser-sync').create();
+
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+
+// jekyll child process launched
+gulp.task('jekyll-build', function(done) {
+  return cp.spawn(jekyll, ['build'], {stdio: 'inherit'}).on('close', done);
+});
+
+// rebuild jekyll files
+gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
+  browserSync.reload();
+});
+
+// compile sass & scss
+gulp.task('styles', function() {
+	return gulp
+		.src('_assets/css/main.sass')
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(sass({outputStyle: 'expanded'}))
+		.pipe(autoprefix())
+		.pipe(gulp.dest('_site/assets/css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(gulp.dest('_site/assets/css'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// compile javascript
+gulp.task('scripts', function() {
+	return gulp
+		.src(['_assets/js/vendor/*', '_assets/js/functions.js'])
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(jsHint())
+    .pipe(jsHint.reporter('default'))
+		.pipe(concat('main.js'))
+    .pipe(gulp.dest('_site/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('_site/assets/js'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// optimize images
+gulp.task('images', function() {
+	return gulp
+		.src('_assets/img/*.{jpg,png}')
+		.pipe(cache(imageMin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('_site/assets/img'));
+});
+
+/*
+ you can't build a website without icons
+ drop your icon fonts into a "fonts" folder in the tools dir
+ gulp will take care of the rest
+*/
+gulp.task('icons', function() {
+	return gulp
+		.src('_assets/img/icons/*')
+		.pipe(gulp.dest('_site/assets/img/icons'));
+});
+
+// run primary tasks, start server, and watch files
+gulp.task('serve', ['styles', 'scripts', 'images', 'icons'], function() {
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: '_site'
+		}
+	});
+	gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_posts/*'], ['jekyll-rebuild']);
+	gulp.watch('_assets/css/**/*.{sass,scss}',['styles']);
+	gulp.watch('_assets/js/*.js', ['scripts']);
+	gulp.watch('_assets/img/*.{jpg,png}', ['images']);
+	gulp.watch('_assets/img/icons/*', ['icons']);
+});
+
+/*
+ before production clean any extraneous files
+ and folders from your assets dir by running
+ "gulp clean" then rebuild with "gulp"
+*/
+gulp.task('clean', function() {
+	return del(['_site/assets/*']);
+});
+
+// gulp default task
+gulp.task('default', ['jekyll-build'], function() {
+  gulp.start('serve');
+});
+
+// error handling
+var onError = function (error) {
+	console.log(error.message);
+	this.emit('end');
+};
